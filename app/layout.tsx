fbd8244106aaebda30b4b87c887ba5ce42d36db0
MehulDev0228 <manas.arora0209@gmail.com>
2025-10-20 01:14:56 +0530

    VERCEL WEB ANALYTICS ADD

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono, Playfair_Display } from "next/font/google"
 import "./globals.css"
 import { AuthProvider } from "@/lib/auth-context"
 import { LayoutWrapper } from "@/components/layout-wrapper"
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({
   subsets: ["latin"],
   variable: "--font-geist-sans",
@@ -34,7 +34,7 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} ${playfair.variable}`}>
       <body>
         <AuthProvider>
-          <LayoutWrapper>{children}</LayoutWrapper>
+          <LayoutWrapper>{children} <Analytics /></LayoutWrapper>
         </AuthProvider>
       </body>
     </html>
